8467177cde709ea64497b43378c8c65a3759b80d
DavyCosta701 <davycosta701@gmail.com>
2023-04-21 13:52:37 -0300

    Added Desafio1

---
+++ Sprint7/Desafio1/aws.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from datetime import date
+import boto3
+from botocore.exceptions import ClientError
+
+session = boto3.Session(profile_name="default")
+
+
+def upload_file(file_name: list, bucket: str) -> bool:
+    """_summary_
+    Args:
+        file_name (list): Lista de arquivos a serem enviados para o bucket S3
+        bucket (str): Nome do bucket
+
+    Returns:
+        bool: Retorna False se a operação der erro, caso contrário True
+    """
+    # Upload the file
+    s3_client = session.client("s3")
+    #
+    for i in file_name:
+        try:
+            s3_client.upload_file(
+                i,
+                bucket,
+                f"Raw/Local/{os.path.splitext(i)[1][1:].upper()}/{os.path.splitext(i)[0].upper()}\
+                                  /{str(date.today())[0:4]}/{str(date.today())[5:7]}/{str(date.today())[8:10]}/{i}",
+            )
+        except ClientError as error:
+            logging.error(error)
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    if upload_file(["movies.csv", "series.csv"], "data-lake-desafio-1"):
+        print("Arquivos Upados")
+
+    else:
+        print("Erro no Upload")
